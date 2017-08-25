45298fa94d04b2bb01fa70420f6ccfec2da4bf7c
Alex Herraiz <alexander_herraiz@hotmail.com>
2017-08-25 18:17:06 -0400

    Committing working initial home component and routing

--- acg/src/app/app.module.ts
+++ acg/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -21,6 +23,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    AlertModule.forRoot(),
     RouterModule.forRoot(
       appRoutes, { enableTracing: true }
     )
