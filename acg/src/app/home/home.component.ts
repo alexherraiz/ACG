9ea536de1a81720dae580c8eb4c8e0f1b19118a6
Alex Herraiz <alexander_herraiz@hotmail.com>
2017-08-25 20:15:07 -0400

    Removing CSS and adding LESS

--- acg/src/app/home/home.component.ts
+++ acg/src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Title }     from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
   public constructor(private titleService: Title ) { }
